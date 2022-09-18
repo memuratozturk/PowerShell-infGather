7b46f573a4a5c1518ffa80043dcbd6396f53baca
murat öztürk <71570762+memuratozturk@users.noreply.github.com>
2022-09-18 17:36:08 +0300

    Add files via upload

---
+++ PowerShell-İnfGather.ps1
@@ -0,0 +1,37 @@
+﻿$gurup = get-LocalGroup | Format-List
+echo "----------------------------"
+echo " yerel gruplar "
+echo "----------------------------"
+echo $gurup
+
+
+echo "----------------------------"
+echo "yerel kullanıcılar"
+echo "----------------------------"
+
+$kullanici = Get-LocalUser | Format-List
+echo $kullanici
+
+
+echo "----------------------------"
+echo "çalışan süreçler ve dizinleri : "
+echo "----------------------------"
+
+$servisler = Get-Process | Format-Table -Wrap -AutoSize -Property Path,Name,Id
+echo $servisler
+
+
+echo "----------------------------"
+echo " işlemci bilgileri : "
+echo "----------------------------"
+
+Get-CimInstance -ClassName Win32_Processor | Select-Object -ExcludeProperty "CIM*"
+
+
+echo "                                                      "
+echo "----------------------------"
+echo " windows kayıtlı kullanıcılar : "
+echo "----------------------------"
+
+
+Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -Property NumberOfLicensedUsers,NumberOfUsers,RegisteredUser
